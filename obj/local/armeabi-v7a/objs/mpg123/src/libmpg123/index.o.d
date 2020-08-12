0c04bc5243f55543a8ebc4f05294f3f38df09c2d
maoyuyin <maoyuyin@126.com>
2020-08-12 17:40:38 +0800

    add

---
+++ obj/local/armeabi-v7a/objs/mpg123/src/libmpg123/index.o.d
@@ -0,0 +1,19 @@
+./obj/local/armeabi-v7a/objs/mpg123/src/libmpg123/index.o: \
+  jni/mpg123-1.25.6/src/libmpg123/index.c jni/mpg123-1.25.6/src/intsym.h \
+  jni/mpg123-1.25.6/android/config.h \
+  jni/mpg123-1.25.6/src/libmpg123/index.h \
+  jni/mpg123-1.25.6/src/compat/compat.h \
+  jni/mpg123-1.25.6/src/libmpg123/true.h \
+  jni/mpg123-1.25.6/src/libmpg123/debug.h
+
+jni/mpg123-1.25.6/src/intsym.h:
+
+jni/mpg123-1.25.6/android/config.h:
+
+jni/mpg123-1.25.6/src/libmpg123/index.h:
+
+jni/mpg123-1.25.6/src/compat/compat.h:
+
+jni/mpg123-1.25.6/src/libmpg123/true.h:
+
+jni/mpg123-1.25.6/src/libmpg123/debug.h:
